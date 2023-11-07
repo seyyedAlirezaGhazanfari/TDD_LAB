4b4351076f7f373253c819be8ab6916671c9c42b
seyyedAlirezaGhazanfari <alireza79.ghazanfari@gmail.com>
2023-11-07 16:21:25 +0330

    Undo setters and getters
    Undo setters and getters for step 2.

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -9,23 +9,6 @@ public class Rectangle {
         this.length = length;
     }
 
-
-    public double getWidth() {
-        return width;
-    }
-
-    public void setWidth(double width) {
-        this.width = width;
-    }
-
-    public double getLength() {
-        return length;
-    }
-
-    public void setLength(double length) {
-        this.length = length;
-    }
-
     public double getArea() {
         return this.width * this.length;
     }
