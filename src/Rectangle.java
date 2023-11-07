63438ed9bea785d7a299798d21591913c989fafe
seyyedAlirezaGhazanfari <alireza79.ghazanfari@gmail.com>
2023-11-07 16:09:50 +0330

    Complete Rectangle for test
    Complete class Rectangle for feeding test's requirements.

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,4 +1,27 @@
 package src;
 
 public class Rectangle {
+    private double width;
+    private double length;
+
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getArea() {
+        return this.width * this.length;
+    }
 }
